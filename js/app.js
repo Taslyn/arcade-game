9805fa458f25fd7301ac2922d54447deb80d4c88
Tabea Brodhagen <tabea_brodhagen@yahoo.de>
2018-06-10 21:07:08 +0200

    feat: Implementation of enemy-player-collision
    Reset of player to starting position after collision with enemy.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
-// Enemies our player must avoid
-/*var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};*/
-
+// function to create a random integer in a given range
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -30,33 +8,31 @@ class Enemy {
     constructor() {
         const startPositions = [60, 145, 230];
         this.sprite = 'images/enemy-bug.png';
-        this.x = 0;
-        this.y = startPositions[getRandomInt(0,2)]; //60 145 230 (+85)
+        this.x = 22;
+        this.y = startPositions[getRandomInt(0,2)]; 
         this.speed = getRandomInt(1,10);
     }
     update(dt) {
         this.x += this.speed;
+        // check for collision with player:
+        if (this.y == player.y && this.x >= (player.x-87) && this.x <= player.x) {
+            player.initializePlayer(); //bug touches player at player.x - 78
+        }
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-/* about game field:
-for Player: 59 < y < 401; -1 < x < 401
-*/
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// The player
 class Player {
   constructor() {
     this.sprite = 'images/char-boy.png';
     this.initializePlayer();
   }
   initializePlayer() {
-    this.x = 300;
-    this.y = 400; 
+    this.x = 300; 
+    this.y = 400;  
   }
   update(dt) {
   }
@@ -92,14 +68,14 @@ class Player {
 }
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 let allEnemies = [];
 
 setInterval(function addEnemy() {
     allEnemies.push(new Enemy());
 }, 1000);
 
+// remove enemies that are outside of game board:
 setInterval(function() {
     allEnemies.forEach(function removeUnnecessaryEnemies(enemy, i) {
         if (enemy.x > 600) {
@@ -113,8 +89,6 @@ setInterval(function() {
 let player = new Player();
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
